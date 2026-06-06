409461c3ce58d14b1b9ac33a89a4e76e4409294c
rishugoyal9923-glitch <rishugoyal9923@gmail.com>
2026-06-07 00:51:50 +0530

    Add files via upload

---
+++ tut2.cpp
@@ -0,0 +1,22 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a=14;
+//     int b=15;
+//     cout<<"this is tutorial 4.here the value of a is "<<a<<".the value of b is "<<b;
+//     return 0;
+// }
+
+    
+#include<iostream>
+using namespace std;
+int main(){
+    int a=14,b=15;
+    float pi=3.14;
+    char c='u';
+    cout<<"this is tutorial 4\n here the value of a is"<<a<<"\n the value of b is "<<b;
+    cout<<"\n the value of pi is:"<<pi;
+    cout<<"\n the value of c is:"<<c;
+    return 0;
+}
+
